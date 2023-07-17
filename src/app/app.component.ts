3c5a7f3c90430e66559584e3ce86083c813d7ffb
Samson Kamau Mwangi <samsonkamau316@gmail.com>
2023-07-17 15:58:01 +0300

    payment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'payment-test';
+  paymentHandler: any = null;
+  constructor() {}
+  ngOnInit() {
+    this.invokeStripe();
+}
+makePayment(amount: any) {
+  const paymentHandler = (<any>window).StripeCheckout.configure({
+    key: 'pk_test_51NTJxnE1kz0StBBcSnsccw1h6DHtzdJlfEubrdbICxyGgnnK0xsUSYT5BcbPnnlHWx8xGGtXPdWZfWbnAa3UVo36009Eu5NPUC',
+    locale: 'auto',
+    token: function (stripeToken: any) {
+      console.log(stripeToken);
+      alert('Stripe token generated!');
+    },
+  });
+  paymentHandler.open({
+    name: 'Positronx',
+    description: '3 widgets',
+    amount: amount * 100,
+  });
+}
+invokeStripe() {
+  if (!window.document.getElementById('stripe-script')) {
+    const script = window.document.createElement('script');
+    script.id = 'stripe-script';
+    script.type = 'text/javascript';
+    script.src = 'https://checkout.stripe.com/checkout.js';
+    script.onload = () => {
+      this.paymentHandler = (<any>window).StripeCheckout.configure({
+        key: 'pk_test_51NTJxnE1kz0StBBcSnsccw1h6DHtzdJlfEubrdbICxyGgnnK0xsUSYT5BcbPnnlHWx8xGGtXPdWZfWbnAa3UVo36009Eu5NPUC',
+        locale: 'auto',
+        token: function (stripeToken: any) {
+          console.log(stripeToken);
+          alert('Payment has been successfull!');
+        },
+      });
+    };
+    window.document.body.appendChild(script);
+  }
+}
 }
